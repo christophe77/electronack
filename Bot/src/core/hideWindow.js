2dae6431024a1fa00107fa92df8381ba2ebfb753
christophe bellec <christophe.bellec@ekim.fr>
2022-01-01 17:37:38 +0100

    fix hide window

--- Bot/src/core/hideWindow.js
+++ Bot/src/core/hideWindow.js
@@ -1,22 +1,25 @@
-const ref = require('ref-napi');
-const ffi = require('ffi-napi');
+const ref = require("ref-napi");
+const ffi = require("ffi-napi");
 
 const voidPtr = ref.refType(ref.types.void);
+const stringPtr = ref.refType(ref.types.CString);
 
-const user32 = ffi.Library('user32.dll', {
-    EnumWindows: ['bool', [voidPtr, 'int32']],
-    ShowWindow: ['int', ['int', 'int']]
+const user32 = ffi.Library("user32.dll", {
+  EnumWindows: ["bool", [voidPtr, "int32"]],
+  GetWindowTextA: ["long", ["long", stringPtr, "long"]],
+  ShowWindow: ["int", ["int", "int"]],
 });
 
 function hideWindow(windowName) {
-    const windowProc = ffi.Callback('bool', ['long', 'int32'], (hwnd) => {
-        const buf = new Buffer.alloc(255);
-        const name = ref.readCString(buf, 0);
-        if (name.includes(windowName)) {
-            user32.ShowWindow(hwnd, 0);
-        }
-    });
-    user32.EnumWindows(windowProc, 0);
+  windowProc = ffi.Callback("bool", ["long", "int32"], function (hwnd, lParam) {
+    const buf = new Buffer.alloc(255);
+    const ret = user32.GetWindowTextA(hwnd, buf, 255);
+    const name = ref.readCString(buf, 0);
+    if (ret && name.includes(windowName)) {
+      user32.ShowWindow(hwnd, 0);
+    }
+    return true;
+  });
+  user32.EnumWindows(windowProc, 0);
 }
-
-module.exports = hideWindow
+module.exports = hideWindow;
